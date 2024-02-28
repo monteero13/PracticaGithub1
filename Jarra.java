ca3c3040671014ffc61aa95a8d909151c6308d9f
monteero13 <13albertomontero@gmail.com>
2024-02-28 13:26:22 +0100

    Version final Alberto

--- Jarra.java
+++ Jarra.java
@@ -37,4 +37,5 @@ public class Jarra { //atributos
     public void metodoComunitario(){
     // Comentario incluido para pruebas
     System.out.println("Aqui escribimos todos");
+    System.out.println("Alberto Montero Solera");
     }
